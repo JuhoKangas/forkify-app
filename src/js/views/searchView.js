eed741d9fabdd521b75349d91613306ec3d8b43b
Juho Kangas <juhokn@gmail.com>
2020-06-07 16:13:30 +0300

    add the search function

---
+++ src/js/views/searchView.js
@@ -0,0 +1,92 @@
+import {
+    elements,
+    renderLoader
+} from './base';
+
+export const getInput = () => elements.searchInput.value;
+
+export const clearInput = () => {
+    elements.searchInput.value = '';
+};
+
+export const clearResults = () => {
+    elements.searchResList.innerHTML = '';
+    elements.searchResPages.innerHTML = '';
+};
+
+// Can be used to limit the recipe title to fit in one line
+
+// const limitRecipetitle = (title, limit = 17) => {
+//     const newTitle = [];
+//     if (title.length > limit) {
+//         title.split(' ').reduce((acc, cur) => {
+//             if (acc + cur.length <= limit) {
+//                 newTitle.push(cur);
+//             }
+//             return acc + cur.length;
+//         }, 0);
+
+//         // return the result
+//         return `${newTitle.join(' ')} ...`;
+//     }
+//     return title;
+// };
+
+const renderRecipe = recipe => {
+    const markup = `
+                <li>
+                    <a class="results__link" href="#${recipe.recipe_id}">
+                        <figure class="results__fig">
+                            <img src="${recipe.image_url}" alt="${recipe.title}">
+                        </figure>
+                        <div class="results__data">
+                            <h4 class="results__name">${recipe.title}</h4>
+                            <p class="results__author">${recipe.publisher}</p>
+                        </div>
+                    </a>
+                </li>
+    `;
+    elements.searchResList.insertAdjacentHTML('beforeend', markup);
+};
+
+// type: 'prev' or 'next'
+const createButton = (page, type) => `
+        <button class="btn-inline results__btn--${type}" data-goto=${type === 'prev' ? page - 1 : page + 1}>
+        <span>Page ${type === 'prev' ? page - 1 : page + 1}</span>
+            <svg class="search__icon">
+                <use href="img/icons.svg#icon-triangle-${type === 'prev' ? 'left' : 'right'}"></use>
+            </svg>
+        </button>                
+`;
+
+const renderButtons = (page, numOfResults, resPerPage) => {
+    const pages = Math.ceil(numOfResults / resPerPage);
+    let button;
+
+    if (page === 1 && pages > 1) {
+        // Show only button going to the next page
+        button = createButton(page, 'next');
+    } else if (page < pages) {
+        // Show both buttons
+        button = `
+            ${createButton(page, 'prev')}
+            ${createButton(page, 'next')}
+        `;
+    } else if (page === pages && pages > 1) {
+        // Show only button going to the previous page
+        button = createButton(page, 'prev');
+    }
+
+    elements.searchResPages.insertAdjacentHTML('afterbegin', button);
+};
+
+export const renderResults = (recipes, page = 1, resPerPage = 10) => {
+    // Render the results of current page
+    const start = (page - 1) * resPerPage; // will work dynamically even if more results are added in the future to the API
+    const end = page * resPerPage;
+
+    recipes.slice(start, end).forEach(renderRecipe);
+
+    // Render the pagination buttons
+    renderButtons(page, recipes.length, resPerPage);
+};
