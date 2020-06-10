f47e1302c87c8b6745acae149d3e72083238a8e0
Juho Kangas <juhokn@gmail.com>
2020-06-10 14:02:19 +0300

    Add the shopping list functionality

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,9 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
+import List from './models/List';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
+import * as listView from './views/listView';
 import {
     elements,
     renderLoader,
@@ -101,6 +103,43 @@ const controlRecipe = async () => {
 
 ['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
 
+
+/**
+ * List Controller
+ */
+
+const controlList = () => {
+    //Create a new list IF there is none yet
+    if (!state.list) state.list = new List();
+
+    //Add each ingredient to the list and UI
+    state.recipe.ingredients.forEach(e => {
+        const item = state.list.addItem(e.count, e.unit, e.ingredient);
+        listView.renderItem(item);
+    });
+};
+
+// Handle delete and update list item events
+elements.shopping.addEventListener('click', e => {
+    const id = e.target.closest('.shopping__item').dataset.itemid;
+
+
+    if (e.target.matches('.shopping__delete, .shopping__delete *')) {
+
+        // Delete the item from the data
+        state.list.deleteItem(id);
+
+        // Delete the item from the UI
+        listView.deleteItem(id);
+
+        //Handle the count update
+    } else if (e.target.matches('.shopping__count-value')) {
+        const val = parseFloat(e.target.value);
+        state.list.updateCount(id, val);
+    }
+});
+
+
 // Handles the recipe button clicks
 elements.recipe.addEventListener('click', e => {
     if (e.target.matches('.btn-decrease, .btn-decrease *')) {
@@ -112,6 +151,7 @@ elements.recipe.addEventListener('click', e => {
     } else if (e.target.matches('.btn-increase, .btn-increase *')) {
         state.recipe.updateServings('inc');
         recipeView.updateServingsIngredients(state.recipe);
+    } else if (e.target.matches('.recipe__btn--add, .recipe__btn--add *')) {
+        controlList();
     }
-    console.log(state.recipe);
 });
